1428963e2dd1e5c329f9015a30194c1914ebebbf
jiseung-gu <tmdrn457@naver.com>
2021-04-20 08:55:03 +0900

    master

---
+++ chaincode.go
@@ -0,0 +1,55 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+// Init is called when the chaincode is instantiated by the blockchain network.
+// params - tokenName, Sysbol, Owner(address), amount
+func (cc *ERC20Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+	_, params := stub.GetFunctionAndParameters()
+	fmt.Println("~~~~Init Called with Params: ~~~~", params)
+
+	return cc.controller.Init(stub, params)
+}
+
+// Invoke is called as a result of an application request to run the chaincode.
+func (cc *ERC20Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+
+	switch fcn {
+	case "totalSupply":
+		return cc.controller.TotalSupply(stub, params)
+	case "balanceOf":
+		return cc.controller.BalanceOf(stub, params)
+	case "transfer":
+		return cc.controller.Transfer(stub, params)
+	case "allowance":
+		return cc.controller.Allowance(stub, params)
+	case "approve":
+		return cc.controller.Approve(stub, params)
+	case "approvalList":
+		return cc.controller.ApprovalList(stub, params)
+	case "transferFrom":
+		return cc.controller.TransferFrom(stub, params)
+	case "transferOtherToken":
+		return cc.controller.TransferOtherToken(stub, params)
+	case "increaseAllowance":
+		return cc.controller.IncreaseAllowance(stub, params)
+	case "decreaseAllowance":
+		return cc.controller.DecreaseAllowance(stub, params)
+	case "mint":
+		return cc.controller.Mint(stub, params)
+	case "burn":
+		return cc.controller.Burn(stub, params)
+	default:
+		return sc.Response{Status: 404, Message: "404 Not Found", Payload: nil}
+	}
+}
---
+++ chaincode_test.go
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package main
+
+var function = []byte("mint")
+
+const txMint = "txMint"
+const tokenName = "dappToken"
+const initAmount = 100000
+const address = "dappcampus"
